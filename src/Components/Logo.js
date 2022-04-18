ba7a00d01993c04c49e865a15fa348a4c4b7b2d3
Ishan-Garg17 <ishangarg17@gmail.com>
2022-04-18 08:46:23 +0530

    installed packages with lower version

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -3,7 +3,6 @@ import Tilty from 'react-tilty';
 import logo from './logo.png'
 const Logo = () => {
     return (
-
         <div  className="ma5 ">
             <Tilty className="Tilt color shadow-3" axis="null" scale={1.5}
                 options={{ max: 55 }} perspective={900} speed={300} reset={true} style={{ height: 120, width: 130,border: '1.5px solid black' }}>
@@ -12,7 +11,6 @@ const Logo = () => {
                 </div>
             </Tilty>
         </div>
-
     )
 
 }
